b231d52acbf7b7551695d423a3d01231b935ea66
isaacmg <igodfried@isaac26.com>
2017-01-28 23:36:34 -0500

    updated + pom + airflow

---
+++ src/main/java/JSONParsing.java
@@ -0,0 +1,5 @@
+/**
+ * Created by img on 1/28/2017.
+ */
+public class JSONParsing {
+}
--- src/main/java/KafkaUSGS.java
+++ src/main/java/KafkaUSGS.java
@@ -1,9 +1,27 @@
 /**
- * Created by img on 1/27/2017.
+ * Created by Isaac Godfried on 1/27/2017.
  */
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class KafkaUSGS {
     public static void main(String [ ] args){
         System.out.println("hello this jar is working");
+        Properties props = initProp("someFile.properties");
+
     }
+    public static Properties initProp(String settingsPath){
+        Properties props = new Properties();
+        return props;
+
+
+
+    }
+
 
 }
