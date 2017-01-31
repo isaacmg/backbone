22e6ebfc201560777efe272a919eaf3ee88a8bea
isaacmg <igodfried@isaac26.com>
2017-01-31 01:40:32 -0500

    updated code
    A bit less sloppy

--- src/main/java/KafkaUSGS.java
+++ src/main/java/KafkaUSGS.java
@@ -6,11 +6,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,19 +20,20 @@ public class KafkaUSGS {
         System.out.println("Welcome to the Kafka jar");
         Properties props = initProp("someFile.properties");
         KafkaProducer<String,String> producer = new KafkaProducer<String,String>(props);
-        //Initialize list of urls of streams. This would probably be loaded from a file or db in production.
+        //Initialize list of urls of streams. This would probably be loaded from a file or directly form a DB in production.
         List<String> s = new ArrayList<String>();
         s.add("https://waterservices.usgs.gov/nwis/iv/?format=json&sites=01037000&parameterCd=00060");
+        s.add("http://waterservices.usgs.gov/nwis/iv/?format=json&sites=14182500&parameterCd=00060");
         //Send Kafka the message
-        String jsonString = getDataString(s);
-        ProducerRecord<String,String> producerRecord = new ProducerRecord<String,String>("test", "IDS", jsonString);
-        producer.send(producerRecord);
+        List<String> jsonString = getDataString(s);
+        for(String message:jsonString){
+            ProducerRecord<String,String> producerRecord = new ProducerRecord<String,String>("test", "a river", message);
+            producer.send(producerRecord);
+        }
         producer.close();
 
-
-
-
     }
+    //Initialize properties for Kafka Producer (optionally load properties from a file)
     private static Properties initProp(String settingsPath){
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
@@ -43,9 +41,11 @@ public class KafkaUSGS {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
         return props;
     }
-    private static String getDataString(List<String> urls){
-        String results = "";
+    //Get list of JSON Strings from USGS API
+    private static List<String> getDataString(List<String> urls){
+        ArrayList<String> completeResults = new ArrayList<String>();
         for(String url:urls) {
+            String results = "";
             try {
                 URL address = new URL(url);
                 BufferedReader reader = new BufferedReader(new InputStreamReader(address.openStream()));
@@ -58,9 +58,10 @@ public class KafkaUSGS {
             } catch (Exception s) {
                 s.printStackTrace();
             }
+            completeResults.add(results);
         }
 
-        return results;
+        return completeResults;
     }
 
 
